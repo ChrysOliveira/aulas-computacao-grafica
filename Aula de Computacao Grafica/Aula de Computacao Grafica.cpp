cd35fd4a0215d14815eaa4de405728c5bc03449d
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2024-09-01 20:38:24 -0300

    adding transformation and z dimension to triangule

--- Aula de Computacao Grafica/Aula de Computacao Grafica.cpp
+++ Aula de Computacao Grafica/Aula de Computacao Grafica.cpp
@@ -5,20 +5,28 @@
 #include <time.h>
 #include <iostream>
 #include <chrono>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
 //Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shaderProgram;
+GLuint VAO, VBO, EBO, shaderProgram;
 
 //Vertex Shader
 static const char* vShader = "                  \n\
 #version 330                                    \n\
                                                 \n\
-layout(location=0) in vec2 pos;                 \n\
+layout(location=0) in vec3 pos;                 \n\
+                                                \n\
+uniform mat4 model;                             \n\
+                                                \n\
+out vec4 vColor;                                \n\
                                                 \n\
 void main(){                                    \n\
-  gl_Position = vec4(pos.x, pos.y, 0.0, 1.0);   \n\
+  gl_Position = model * vec4(pos, 1.0);         \n\
+  vColor = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);  \n\
 }";
 
 //Fragment Shader
@@ -27,23 +35,37 @@ static const char* fShader = "                  \n\
                                                 \n\
 uniform vec3 triangleColor;                     \n\
                                                 \n\
+in vec4 vColor;                                 \n\
 out vec4 color;                                 \n\
                                                 \n\
 void main(){                                    \n\
-  color = vec4(triangleColor, 1.0);             \n\
+  color = vColor;                               \n\
 }";
 
 void CreateTriagle() {
     //1. Definir os pontos dos vértices
     GLfloat vertices[] = {
-        -1.0f, -1.0f, //Vértice 1 (x, y)
-        1.0f, -1.0f,  //Vértice 2 (x, y)
-        0.0f, 1.0f    //Vértice 3 (x, y)
+        -1.0f, -1.0f, 0.0f, //Vertice 1 (x, y, z)
+		0.0f, 1.0f, 0.0f,   //Vertice 2 (x, y, z)
+		1.0f, -1.0f, 0.0f,	//Vertice 3 (x, y, z)
+		0.0f, 0.0f, 1.0f    //Vertice 3 (x, y, z)
     };
+
+    GLuint indices[] = {
+		0, 1, 2,
+		0, 1, 3,
+		0, 2, 3,
+		1, 2, 3
+	};
 
     //VAO
     glGenVertexArrays(1, &VAO); //Gera um VAO ID
     glBindVertexArray(VAO); //Atribuindo o ID ao VAO
+
+    // EBO
+    glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     //Carregar os dados de vértice para a placa de vídeo
     //Vertex Buffer Object: VBO
@@ -56,13 +78,14 @@ void CreateTriagle() {
     //GL_STREAM_DRAW : Os dados do vértice serão carregados uma vez e desenhados uma vez.
 
     //Vertex Attribute Pointer - Atributos dos dados na memória
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     //0: shader location | 2: numero de valores de vertice (x,y) | GL_FLOAT: tipo dos valores
     //GL_FALSE: normalizado | 0: pular elemento (cor) | 0: offset
 //Vertex Attribute Pointer Enable
     glEnableVertexAttribArray(0); //0: shader location
 
     //Limpar o Buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     //Limpar o Vertex Array
@@ -137,7 +160,8 @@ void CompileShaders() {
 
 int main()
 {
-    //auto t_start = std::chrono::high_resolution_clock::now();
+    auto t_start = std::chrono::high_resolution_clock::now();
+
     //initialize GLFW
     if (!glfwInit()) {
         printf("GLFW Inicialization fail");
@@ -145,6 +169,7 @@ int main()
         return 1;
     }
 
+    glEnable(GL_DEPTH_TEST);
     //Setting GLFW window propeties
     //OpenGL Version
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -185,29 +210,69 @@ int main()
     CreateTriagle();
     CompileShaders();
 
+	bool direction = true, sizeDirection = true, angleDirection = true;//true = direita / false = esquerda
+	float triOffset = 0.0f, maxOffset = 0.7f, minOffset = -0.7f, incOffset = 0.01f;
+	float size = 0.0f, maxSize = 0.7f, minSize = -0.7f, incSize = 0.01f;
+	float angle = 0.0f, maxAngle = 360.0f, minAngle = -1.0f, incAngle = 0.5f;
+
     //Loop until the window close
     while (!glfwWindowShouldClose(mainWindow)) {
         //Get + Handle user input events
         glfwPollEvents();
 
         //Clear Window (100% = 255)
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.52f, 0.36f, 0.87f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //Desenhar o triangulo
         glUseProgram(shaderProgram); //Busca o programa que está com o shader (triangulo)
         glBindVertexArray(VAO); //Bind o VAO
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
         GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor"); //procura a entrada chamada triangleColor
-        //float r = (float)rand()/RAND_MAX;
-        //float g = (float)rand()/RAND_MAX;
-        //float b = (float)rand()/RAND_MAX;
-        //glUniform3f(uniColor, r, g, b); //atualiza o valor da entrada com a cor vermelha
+        float r = (float)rand()/RAND_MAX;
+        float g = (float)rand()/RAND_MAX;
+        float b = (float)rand()/RAND_MAX;
+        glUniform3f(uniColor, r, g, b); //atualiza o valor da entrada com cor aleatoria
 
         //auto t_now = std::chrono::high_resolution_clock::now();
         //float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
         //glUniform3f(uniColor, (sin(time * 4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
 
-        glUniform3f(uniColor, 1.0f, 0.0f, 0.0f); //atualiza o valor da entrada com a cor vermelha
+        // Move triangulo
+        
+		if (triOffset >= maxOffset || triOffset <= minOffset)
+			direction = !direction;
+		triOffset += direction ? incOffset : incOffset * -1;
+
+		if (size >= maxSize || size <= minSize)
+			sizeDirection = !sizeDirection;
+		size += sizeDirection ? incSize : incSize * -1;
+
+		if (angle >= maxAngle || angle <= minAngle)
+			angleDirection = !angleDirection;
+		angle += angleDirection ? incAngle : incAngle * -1;
+
+		//criar matriz
+		glm::mat4 model(1.0f);
+
+		//Movimentações do triangulo
+		model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+
+		//Tamanho do triangulo
+		model = glm::scale(model, glm::vec3(0.4, 0.4, 0.4));
+
+		//Rotação
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		GLint uniModel = glGetUniformLocation(shaderProgram, "model");
+		glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+
+
+        //glUniform3f(uniColor, 1.0f, 0.0f, 0.0f); //atualiza o valor da entrada com a cor vermelha
         glDrawArrays(GL_TRIANGLES, 0, 3); //Desenha um triangulo | 0: Array index | 2: Número de pontos (vértices)
         glBindVertexArray(0);
         glUseProgram(0); //remove o programa da memória
